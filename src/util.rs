47bf228ab741f6eaee0f0a05d862da6b3d95a62b
PatrickTorgerson <TorgersonPatrickS@gmail.com>
2020-12-01 14:07:31 -0800

    Addes Pos Utility: Pos::from() , pos.valid()

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,7 @@
 pub struct Pos{
     pub rank:i32,
     pub file:i32,
-    
+
 }
 
 impl Pos
@@ -10,5 +10,23 @@ impl Pos
     {
         return Pos{rank,file};
     }
+    pub fn from(square:&String) -> Self
+    {
+        let rank = square.chars().nth(0).unwrap();
+        let file = square.chars().nth(1).unwrap();
 
+        if rank < 'a' || rank > 'h' || file < '1' || file > '8'
+        {
+            return Pos {rank:-1,file:-1};
+        }
+
+        return Pos {
+            rank : (rank as i32) - 97,                      // 97 is the integer representation of 'a'
+            file : (file.to_digit(10).unwrap() as i32) -1   // Minus one because arrays start at 0
+        };
+    }
+    pub fn valid(&self) -> bool
+    {
+        return self.rank > 0 || self.file > 0;
+    }
 }
