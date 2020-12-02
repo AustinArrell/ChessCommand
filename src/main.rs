7bbd0fc297455061f4e4ab59a7ac3ef04829dac6
middleman <middleman@pop-os.localdomain>
2020-12-01 16:28:07 -0800

    Create Game.rs and begin implementaion

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
 
 mod piece;
 mod util;
+mod game;
 
 mod pawn;
 mod rook;
@@ -25,9 +26,9 @@ use queen::Queen;
 
 use piece::Piece;
 use util::Pos;
+use game::Game;
 
-use std::io;
-use std::io::Write;
+
 
 /*
 Rank: A-H Queen - King
@@ -46,58 +47,9 @@ fn main()
     let k = King::new(true);
     let q = Queen::new(true);
 
-    let running = true;
-    while running
-    {
-        let mut input = String::new();
+    let game = Game::new(true,true);
+    game.run();
 
-        print!("Enter starting square: ");
-
-        // ensure 'Enter move: ' is printed before we pause for user input
-        io::stdout().flush().expect("Console write error");
-
-        io::stdin().read_line(&mut input).expect("Could not read line!");
-
-        let start_pos = Pos::from(&input);
-
-        if !start_pos.valid()
-        { continue; }
-
-        input.clear();
-        print!("Enter move: ");
-
-        // ensure 'Enter move: ' is printed before we pause for user input
-        io::stdout().flush().expect("Console write error");
-
-        io::stdin().read_line(&mut input).expect("Could not read line!");
-        
-        let piece =
-        if input.chars().next().unwrap().is_uppercase()
-        { input.chars().next().unwrap() }
-        else
-        { 'P' };
-
-        let destination =
-        if input.chars().next().unwrap().is_uppercase()
-        { input[1..].to_string() }
-        else
-        { input };
-
-        let dest_pos = Pos::from(&destination);
-
-        if !dest_pos.valid()
-        { continue; }
     
-        match piece
-        {
-            'Q' => println!("{}", q.verify_move(&start_pos, &dest_pos)),
-            'K' => println!("{}", k.verify_move(&start_pos, &dest_pos)),
-            'R' => println!("{}", r.verify_move(&start_pos, &dest_pos)),
-            'B' => println!("{}", b.verify_move(&start_pos, &dest_pos)),
-            'N' => println!("{}", n.verify_move(&start_pos, &dest_pos)),
-            'P' => println!("{}", p.verify_move(&start_pos, &dest_pos)),
-            _   => println!("invalid piece!")
-        }
-    }
 
 }
